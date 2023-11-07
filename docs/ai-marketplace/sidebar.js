d5d64dcfdfaef16bb066c1169fe48c37faaf4588
benzuspalukj <supalukj_pro@vistec.ac.th>
2023-11-07 19:17:23 +0700

    initial project

---
+++ docs/ai-marketplace/sidebar.js
@@ -0,0 +1,94 @@
+module.exports = [
+  // { type: "doc", id: "petstore/swagger-petstore-yaml" },
+  {
+    type: "doc",
+    id: "ai-marketplace/ocr-documentation",
+    label: "OCR Documentation",
+    className: "api-method post",
+  },
+  {
+    type: "doc",
+    id: "ai-marketplace/ocr-passport",
+    label: "OCR Passport",
+    className: "api-method post",
+  },
+  {
+    type: "doc",
+    id: "ai-marketplace/ocr-thai-id-card",
+    label: "OCR Thai ID Card",
+    className: "api-method post",
+  },
+  // {
+  //   type: "doc",
+  //   id: "ai-marketplace/find-pets-by-status",
+  //   label: "find-pets-by-status",
+  //   className: "api-method post",
+  // },
+  // {
+  //   type: "doc",
+  //   id: "ai-marketplace/upload-file",
+  //   label: "upload-file",
+  //   className: "api-method post",
+  // },
+  {
+    type: "category",
+    label: "OCR Documentation",
+    link: { type: "doc", id: "ai-marketplace/ocr-documentation-info" },
+    items: [
+      // {
+      //   type: "doc",
+      //   id: "petstore/add-pet",
+      //   label: "Add a new pet to the store",
+      //   className: "api-method post",
+      // },
+      // {
+      //   type: "doc",
+      //   id: "petstore/update-pet",
+      //   label: "Update an existing pet",
+      //   className: "api-method put",
+      // },
+      // {
+      //   type: "doc",
+      //   id: "petstore/get-pet-by-id",
+      //   label: "Find pet by ID",
+      //   className: "api-method get",
+      // },
+      // {
+      //   type: "doc",
+      //   id: "petstore/update-pet-with-form",
+      //   label: "Updates a pet in the store with form data",
+      //   className: "api-method post",
+      // },
+      // {
+      //   type: "doc",
+      //   id: "petstore/delete-pet",
+      //   label: "Deletes a pet",
+      //   className: "api-method delete",
+      // },
+      // {
+      //   type: "doc",
+      //   id: "petstore/upload-file",
+      //   label: "Inference API 1",
+      //   className: "api-method post",
+      // },
+      // {
+      //   type: "doc",
+      //   id: "petstore/find-pets-by-status",
+      //   label: "Finds Pets by status",
+      //   className: "api-method get",
+      // },
+      // {
+      //   type: "doc",
+      //   id: "petstore/find-pets-by-tags",
+      //   label: "Finds Pets by tags",
+      //   className: "menu__list-item--deprecated api-method get",
+      // },
+      // {
+      //   type: "doc",
+      //   id: "petstore/new-pet",
+      //   label: "New pet",
+      //   className: "api-method event",
+      // },
+    ],
+  },
+];
