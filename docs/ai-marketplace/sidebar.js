63e9d42c2f6f18aaafefe2cd8464212d15ab0d8a
benzuspalukj <supalukj_pro@vistec.ac.th>
2025-02-25 17:08:09 +0700

    update: remove ocr receipt from menu

--- docs/ai-marketplace/sidebar.js
+++ docs/ai-marketplace/sidebar.js
@@ -110,12 +110,6 @@ module.exports = [
   },
   {
     type: "doc",
-    id: "ai-marketplace/ocr-receipt",
-    label: "OCR Receipt",
-    className: "api-method post",
-  },
-  {
-    type: "doc",
     id: "ai-marketplace/ocr-license-plate",
     label: "OCR License Plate",
     className: "api-method post",
