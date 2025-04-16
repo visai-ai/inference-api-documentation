7254f77c4f1d952a72329d8260933bde85ff34c8
benzuspalukj <supalukj_pro@vistec.ac.th>
2025-04-16 15:48:33 +0700

    feat: add ocr long run do c

--- docs/ai-marketplace/sidebar.js
+++ docs/ai-marketplace/sidebar.js
@@ -6,11 +6,65 @@ module.exports = [
     label: "Machine Translation",
     className: "api-method post",
   },
+  // {
+  //   type: "doc",
+  //   id: "ai-marketplace/ocr-documentation",
+  //   label: "OCR Documentation",
+  //   className: "api-method post",
+  // },
   {
-    type: "doc",
-    id: "ai-marketplace/ocr-documentation",
+    type: "category",
     label: "OCR Documentation",
-    className: "api-method post",
+    link: {
+      type: "generated-index",
+      title: "OCR Documentation",
+      description: "OCR Documentation Inference API",
+      slug: "/ai-marketplace/ocr-documentation",
+    },
+    items: [
+      {
+        type: "doc",
+        id: "ai-marketplace/ocr-documentation/synchronous-api",
+        label: "Synchronous API",
+        className: "api-method post",
+      },
+      {
+        type: "category",
+        label: "Asynchronous API",
+        link: {
+          type: "generated-index",
+          title: "Asynchronous API",
+          description: "Asynchronous - Speech to Text Inference API",
+          slug: "/ai-marketplace/ocr-documentation/asynchronous",
+        },
+        items: [
+          {
+            type: "doc",
+            id: "ai-marketplace/ocr-documentation/asynchronous-api/uploading-file",
+            label: "Uploading files",
+            className: "api-method post",
+          },
+          {
+            type: "doc",
+            id: "ai-marketplace/ocr-documentation/asynchronous-api/get-job-status",
+            label: "Get job status by ID",
+            className: "api-method get",
+          },
+          {
+            type: "doc",
+            id: "ai-marketplace/ocr-documentation/asynchronous-api/list-jobs",
+            label: "List all jobs",
+            className: "api-method get",
+          },
+          {
+            type: "doc",
+            id: "ai-marketplace/ocr-documentation/asynchronous-api/cancel-job",
+            label: "Cancel job",
+            className: "api-method get",
+          },
+        ],
+      },
+    ],
   },
   {
     type: "category",
